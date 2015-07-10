cf8cdca00a2d821d24782fb5c0305df4a1087e3a
Daniel Moran <dmj@tid.es>
2015-07-10 21:02:29 +0200

    Add auxiliary functions for classical factoring

---
+++ src/qbitos/rsaUtils.clj
@@ -0,0 +1,25 @@
+(ns qbitos.rsaUtils
+  (:gen-class))
+
+
+(use 'clojure.set)
+
+
+(defn divisors [n]
+  (set (filter #(= 0 (mod n %)) (range 1 (inc n)))))
+
+(defn prime [n]
+  (= 1 (count (divisors n))))
+
+(defn Gn [n]
+  (conj (set (filter #(= 1 (count (intersection (divisors n) (divisors %)))) (range 1 n))) 1))
+
+
+(defn getPowers
+  ([n N]   (cons n (getPowers (mod (* n n) N) n N)))
+  ([n base N] (cons n (lazy-seq (getPowers (mod (* n base) N) base N))))
+  )
+
+(defn getRange [n N]
+  (inc (count (take-while #(not= % 1) (getPowers n N)))))
+
